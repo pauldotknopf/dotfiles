52caa94c61c4f9ea4dd21f104fdbfb0cbce6a1f2
Paul Knopf <pauldotknopf@gmail.com>
2018-12-04 11:50:27 -0500

    Adding desktop shortcut.

--- scripts/install-rider.sh
+++ scripts/install-rider.sh
@@ -3,3 +3,22 @@
 rm -rf ~/apps/rider
 mkdir -p ~/apps/rider
 curl -L https://download.jetbrains.com/rider/JetBrains.Rider-2018.2.3.tar.gz | tar -xz -C ~/apps/rider
+
+# Create the desktop icon
+rm -rf ~/.local/share/applications/jetbrains-rider.desktop 
+mkdir -p ~/.local/share/applications
+
+cat >~/.local/share/applications/jetbrains-rider.desktop  <<EOL
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=JetBrains Rider
+Icon=$HOME/apps/rider/JetBrains Rider-2018.2.3/bin/rider.png
+Exec="$HOME/apps/rider/JetBrains Rider-2018.2.3/bin/rider.sh" %f
+Comment=The Drive to Develop
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=jetbrains-rider
+EOL
+
+chmod +x ~/.local/share/applications/jetbrains-rider.desktop
