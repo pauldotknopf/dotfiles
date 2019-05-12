cb74b170a86c21e3ed70ee33bac69351b4d94a60
Paul Knopf <pauldotknopf@gmail.com>
2019-05-12 00:26:11 -0400

    Using hstr for command history.

---
+++ zsh-config/hstr.zsh
@@ -0,0 +1,3 @@
+alias hh=hstr                     # hh to be alias for hstr
+export HSTR_CONFIG=hicolor        # get more colors
+bindkey -s "\C-r" "\eqhstr\n"     # bind hstr to Ctrl-r (for Vi mode check doc)
