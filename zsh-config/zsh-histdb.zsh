b6a60291e87b636bce6231a6600b65ea725fad14
Paul Knopf <pauldotknopf@gmail.com>
2019-05-30 21:39:50 -0400

    Using zsh-histdb.

---
+++ zsh-config/zsh-histdb.zsh
@@ -0,0 +1,15 @@
+source $DOTFILES_DIR/3rdparty/zsh-histdb/sqlite-history.zsh
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd histdb-update-outcome
+
+_zsh_autosuggest_strategy_histdb_top() {
+    local query="select commands.argv from
+history left join commands on history.command_id = commands.rowid
+left join places on history.place_id = places.rowid
+where commands.argv LIKE '$(sql_escape $1)%'
+group by commands.argv
+order by places.dir != '$(sql_escape $PWD)', count(*) desc limit 1"
+    suggestion=$(_histdb_query "$query")
+}
+
+ZSH_AUTOSUGGEST_STRATEGY=histdb_top
