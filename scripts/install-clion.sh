f0c9bf0d6cc3921654eb61dd42442122751a47ff
Paul Knopf <pauldotknopf@gmail.com>
2019-08-25 10:19:45 -0400

    Minor changes.

--- scripts/install-clion.sh
+++ scripts/install-clion.sh
@@ -4,22 +4,4 @@ set -e
 rm -rf ~/apps/clion
 mkdir -p ~/apps/clion
 curl -L https://download.jetbrains.com/cpp/CLion-2019.2.tar.gz | tar -xz -C ~/apps/clion
-
-# Create the desktop icon
-rm -rf ~/.local/share/applications/jetbrains-clion.desktop 
-mkdir -p ~/.local/share/applications
-
-cat >~/.local/share/applications/jetbrains-clion.desktop  <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=JetBrains CLion
-Icon=$HOME/apps/clion/clion-2019.2/bin/clion.png
-Exec="$HOME/apps/clion/clion-2019.2/bin/clion.sh" %f
-Comment=The Drive to Develop
-Categories=Development;IDE;
-Terminal=false
-StartupWMClass=jetbrains-clion
-EOL
-
-chmod +x ~/.local/share/applications/jetbrains-clion.desktop
+mv ~/apps/clion/* ~/apps/clion/current
--- scripts/install-rider.sh
+++ scripts/install-rider.sh
@@ -4,22 +4,4 @@ set -e
 rm -rf ~/apps/rider
 mkdir -p ~/apps/rider
 curl -L https://download.jetbrains.com/rider/JetBrains.Rider-2019.1.3.tar.gz | tar -xz -C ~/apps/rider
-
-# Create the desktop icon
-rm -rf ~/.local/share/applications/jetbrains-rider.desktop 
-mkdir -p ~/.local/share/applications
-
-cat >~/.local/share/applications/jetbrains-rider.desktop  <<EOL
-[Desktop Entry]
-Version=1.0
-Type=Application
-Name=JetBrains Rider
-Icon=$HOME/apps/rider/JetBrains Rider-2019.1.3/bin/rider.png
-Exec="$HOME/apps/rider/JetBrains Rider-2019.1.3/bin/rider.sh" %f
-Comment=The Drive to Develop
-Categories=Development;IDE;
-Terminal=false
-StartupWMClass=jetbrains-rider
-EOL
-
-chmod +x ~/.local/share/applications/jetbrains-rider.desktop
+mv ~/apps/rider/* ~/apps/rider/current
