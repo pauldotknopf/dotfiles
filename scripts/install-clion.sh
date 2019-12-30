596df44977f4907f52e11cfa83c43494bb48c4cb
Paul Knopf <pauldotknopf@gmail.com>
2019-12-29 22:09:35 -0500

    Updating JetBrains.

--- scripts/install-clion.sh
+++ scripts/install-clion.sh
@@ -3,5 +3,5 @@ set -e
 
 rm -rf ~/apps/clion
 mkdir -p ~/apps/clion
-curl -L https://download.jetbrains.com/cpp/CLion-2019.2.tar.gz | tar -xz -C ~/apps/clion
+curl -L https://download.jetbrains.com/cpp/CLion-2019.3.2.tar.gz | tar -xz -C ~/apps/clion
 mv ~/apps/clion/* ~/apps/clion/current
--- scripts/install-datagrip.sh
+++ scripts/install-datagrip.sh
@@ -3,5 +3,5 @@ set -e
 
 rm -rf ~/apps/datagrip
 mkdir -p ~/apps/datagrip
-curl -L https://download.jetbrains.com/datagrip/datagrip-2019.2.1.tar.gz | tar -xz -C ~/apps/datagrip
+curl -L https://download.jetbrains.com/datagrip/datagrip-2019.3.1.tar.gz | tar -xz -C ~/apps/datagrip
 mv ~/apps/datagrip/* ~/apps/datagrip/current
