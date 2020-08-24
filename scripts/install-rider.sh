9c24c159074e7d21dc4d3ead4539c2c90213d2ab
Paul Knopf <pauldotknopf@gmail.com>
2020-08-24 11:49:33 -0400

    Updating rider.

--- scripts/install-rider.sh
+++ scripts/install-rider.sh
@@ -3,5 +3,5 @@ set -e
 
 rm -rf ~/apps/rider
 mkdir -p ~/apps/rider
-curl -L https://download.jetbrains.com/rider/JetBrains.Rider-2020.1.4.tar.gz | tar -xz -C ~/apps/rider
+curl -L https://download.jetbrains.com/rider/JetBrains.Rider-2020.2.1.tar.gz | tar -xz -C ~/apps/rider
 mv ~/apps/rider/* ~/apps/rider/current
