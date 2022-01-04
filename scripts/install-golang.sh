900340a1f3fe823a614a675687f0e6cc14027b36
Paul Knopf <pauldotknopf@gmail.com>
2022-01-04 10:06:42 -0500

    Using go 1.14

--- scripts/install-golang.sh
+++ scripts/install-golang.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-curl -L https://golang.org/dl/go1.17.2.linux-amd64.tar.gz | sudo tar -C /usr/local -xzf -
+curl -L https://go.dev/dl/go1.14.15.linux-amd64.tar.gz | sudo tar -C /usr/local -xzf -
 sudo rm /usr/bin/go || true
 sudo ln -s /usr/local/go/bin/go /usr/bin/go
