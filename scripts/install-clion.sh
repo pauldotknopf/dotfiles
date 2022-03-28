fea01329fddf7cf0ef1d2ca8203a33b413b23284
Paul Knopf <pauldotknopf@gmail.com>
2022-03-28 11:46:10 -0400

    Updating clion.

--- scripts/install-clion.sh
+++ scripts/install-clion.sh
@@ -3,5 +3,5 @@ set -e
 
 rm -rf ~/apps/clion
 mkdir -p ~/apps/clion
-curl -L https://download.jetbrains.com/cpp/CLion-2019.3.2.tar.gz | tar -x -C ~/apps/clion
+curl -L https://download.jetbrains.com/cpp/CLion-2021.3.3.tar.gz | tar -xz -C ~/apps/clion
 mv ~/apps/clion/* ~/apps/clion/current
